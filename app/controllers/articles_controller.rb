824d5da1a21237fbd3b767801a79ff3cdb7077b1
Abayomi Yusuf <yusufabayomi27@yahoo.com>
2015-12-29 17:15:02 +0000

    Fixing

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,14 @@
 class ArticlesController < ApplicationController
+  def index
+    @article = Article.all  
+  end
+  
   def new
     @article = Article.new
   end
   
   def create
     @article = Article.new(article_params)
-    @article.save
     if @article.save
       flash[:notice] = "Article was successfully created."
        redirect_to article_path(@article)
@@ -17,6 +20,21 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+  
+  def edit
+    @article = Article.find(params[:id])
+  end
+  
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Article was successfully updated"
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
+  end
+  
    
  private
    def article_params
